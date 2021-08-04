a27428262393a05488f7ec4570e0f276d7cbc8a5
eafifa08 <eafifa07@yandex.ru>
2021-08-04 15:12:44 +0500

    2021-08-04

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
 # Simple ContactBook - Простая книга контактов
 import tkinter as tk
+import sqlite3
+
+
+def create_table():
+    pass
+
+
+def init_database():
+    try:
+        sqlite_connection = sqlite3.connect('contacts.sqlite')
+        cursor = sqlite_connection.cursor()
+        print('База данных создана и успешно подключена')
+
+        query_select_version = "select sqlite_version();"
+        query_create_table = 'CREATE TABLE IF NOT EXISTS contacts' \
+                             ' (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT);'
+        cursor.execute(query_select_version)
+        record = cursor.fetchall()
+        print('Версия базы данных SQLite:', record)
+        cursor.execute(query_create_table)
+        cursor.close()
+    except sqlite3.Error as error:
+        print('Ошибка при подключении к sqlite', error)
+    finally:
+        if(sqlite_connection):
+            sqlite_connection.close()
+            print('Соединение с SQLite закрыто')
 
 
 class Contact:
@@ -12,23 +39,21 @@ class Contact:
 
 
 def start_window():
+    init_database()
     window = tk.Tk()
     window.title("Contact Book")
-
-    button_add = tk.Button(window, text="Добавить")
+    button_add = tk.Button(window, text="Новый контакт")
     button_add.pack()
-
     frame = tk.Frame(borderwidth=5, relief=tk.GROOVE)
     label = tk.Label(master=frame, text="Hello", bg='red')
     label.pack()
     frame.pack()
-
-
     contacts = []
     for i in range(5):
-        contact = Contact(i,i+i)
+        contact = Contact(i, i+i)
         contacts.append(contact)
-    cars = ['vaz', 'mercedes', 'bmw', 'opel']
+    gorgona_contact = Contact('Ж', 8908888741)
+    contacts.append(gorgona_contact)
     listbox = tk.Listbox(window)
     for contact in contacts:
         listbox.insert(tk.END, contact.name)
