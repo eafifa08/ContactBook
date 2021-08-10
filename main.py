6f857a13ab71a44fad13292349be665a53f5106c
neo <eafifa07@yandex.ru>
2021-08-11 00:17:11 +0500

    add_new_contact

--- main.py
+++ main.py
@@ -5,10 +5,6 @@ import sqlite3
 window = tk.Tk()
 
 
-def create_table():
-    pass
-
-
 def insert_contact_to_db(contact):
     try:
         conn = sqlite3.connect('contacts.sqlite')
@@ -26,6 +22,25 @@ def insert_contact_to_db(contact):
             conn.close()
             print('Соединение с SQLite закрыто')
 
+
+def get_all_contacts_from_db():
+    contacts = []
+    try:
+        conn = sqlite3.connect('contacts.sqlite')
+        cursor = conn.cursor()
+        query_get_all_contacts = """SELECT name, tel FROM contacts"""
+        cursor.execute(query_get_all_contacts)
+        records = cursor.fetchall()
+        for record in records:
+            contacts.append(Contact(record[0], record[1]))
+        cursor.close()
+    except sqlite3.Error as error:
+        print('Ошибка при подключении к sqlite', error)
+    finally:
+        if(conn):
+            conn.close()
+            print('Соединение с SQLite закрыто')
+        return contacts
 
 
 def init_database():
@@ -61,17 +76,39 @@ class Contact:
 
 def show_new_contact():
     print('show_new_contact')
-    e_name = tk.Entry(window)
+    e_name = tk.Entry(window, name='name')
     e_name.grid(row=0, column=2)
-    e_tel = tk.Entry(window)
+    e_tel = tk.Entry(window, name='tel')
     e_tel.grid(row=0, column=3)
-    return (e_name, e_tel)
+    btn_save_new_contact = tk.Button(window, text='сохранить', name='to_save')
+    btn_save_new_contact.config(command=save_new_contact)
+    btn_save_new_contact.grid(row=0, column=4)
+    return (e_name, e_tel, btn_save_new_contact)
 
 
-def unshow_new_contact(entries):
-    for entry in entries:
-        entry.grid_remove()
+def unshow_new_contact():
+    elements = [window.nametowidget('tel'), window.nametowidget('name'), window.nametowidget('to_save')]
+    for element in elements:
+        element.grid_remove()
     print('unshow_new_contact')
+
+
+def save_new_contact():
+    name = window.nametowidget('name').get()
+    tel = window.nametowidget('tel').get()
+    print(name)
+    contact = Contact(name, tel)
+    insert_contact_to_db(contact)
+    refresh_listbox()
+    unshow_new_contact()
+
+
+def refresh_listbox():
+    listbox = window.nametowidget('lstbx_contacts')
+    listbox.delete(0, 'end')
+    contacts = get_all_contacts_from_db()
+    for contact in contacts:
+        listbox.insert(tk.END, contact.name)
 
 
 def start_window():
@@ -82,23 +119,20 @@ def start_window():
     button_add.grid(row=0, column=1)
     button_add.bind("button_add", show_new_contact)
     button_add.config(command=show_new_contact)
-    #label = tk.Label(window, text="Hello", bg='red')
-
-    #label.grid(row=0, column=0)
-    #label.pack()
     contacts = []
     for i in range(5):
         contact = Contact(i, str(i) * 8)
         contacts.append(contact)
     gorgona_contact = Contact('Ж', 8908888741)
     contacts.append(gorgona_contact)
-    listbox = tk.Listbox(window)
+    listbox = tk.Listbox(window, name='lstbx_contacts')
+    scrollbar = tk.Scrollbar(listbox, orient='vertical')
     for contact in contacts:
-        listbox.insert(tk.END, contact.name)
         insert_contact_to_db(contact)
+    refresh_listbox()
     listbox.grid(row=0, column=0, rowspan=5)
-    new_entries = show_new_contact()
-    unshow_new_contact(new_entries)
+    show_new_contact()
+    unshow_new_contact()
 
     window.geometry("600x400")
     window.mainloop()
